a0a3140f274c77e6df32919a9fc4ff82439db9c8
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-12-29 21:11:22 +0300

    created configstore.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,54 +1,5 @@
-const PLUS = 'PLUS';
-const MINUS = 'MINUS';
+import { configureStore } from '@reduxjs/toolkit';
 
-const updateState = (state, action) => {
-  if (action.type === 'PLUS') {
-    return { ...state, count: state.count + action.payload };
-  }
-  if (action.type === 'MINUS') {
-    return { ...state, count: state.count - action.payload };
-  }
-  return state;
-};
-
-class Store {
-  constructor(updateState, state) {
-    this._updateState = updateState;
-    this._state = state;
-    this._callBack = [];
-  }
-
-  getState = () => {
-    this._state;
-  };
-
-  dispatch = (action) => {
-    this._state = this._updateState(this._state, action);
-  };
-
-  subscribe = (callBack) => {
-    this._callBack.push(callBack);
-  };
-}
-
-const initState = {
-  count: 0,
-};
-
-const store = new Store(updateState, initState);
-
-store.subscribe(() => console.log('subcribe', store.getState()));
-
-const { dispatch } = store;
-
-const bindActionCreator =
-  (creator, dispatch) =>
-  (...args) => {
-    dispatch(creator(...args));
-  };
-
-const plusAction = (payload) => ({ type: PLUS, payload });
-const minusAction = (payload) => ({ type: MINUS, payload });
-
-const plus = bindActionCreator(plusAction, dispatch);
-const minus = bindActionCreator(minusAction, dispatch);
+export const store = configureStore({
+  sort,
+});
