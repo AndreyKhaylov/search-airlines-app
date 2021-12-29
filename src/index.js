b4d3429b3963d3923f9fcd08c7728e531eacc64c
Andrey Khaylov <andrey.haylovv@yandex.ru>
2021-12-29 21:18:51 +0300

    connected store to app.jsx

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(
+import App from './App';
+import { store } from './store/store';
+
+import './index.css';
+
+const rootElement = document.getElementById('root');
+
+render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  rootElement,
 );
