c51d33f36780062a759f161e801bdbc442a5b829
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-01-14 18:09:56 +0300

    Created filtering by number of flights

--- src/store/reducers/filterReducer.js
+++ src/store/reducers/filterReducer.js
@@ -5,36 +5,35 @@ import { selectData } from './dataReducer';
 export const slice = createSlice({
   name: 'filter',
   initialState: {
-    filteredData: [],
-    filteringBy: {
-      oneTransfer: false,
-      withoutTransfer: false,
-    },
+    data: [],
+    oneTransfer: false,
+    withoutTransfer: false,
   },
   reducers: {
-    filterTransfers: (state, action) => {
+    filterTransfers(state, action) {
       const { value, checked } = action.payload;
-      state.filteringBy[value] = checked;
+      state[value] = checked;
     },
-    setFilteredData: (state, action) => {
-      const data = action.payload;
-      const value = state.filteringBy;
-      switch (value) {
-        case value.oneTransfer === 'true' && value.withoutTransfer === 'true': {
-          const filter = data.filter((i) => i);
-        }
-        case value.oneTransfer === 'true' && value.withoutTransfer === 'false': {
-          const filter = data.filter((i) => i);
-          state.filteredData = action.payload;
-        }
-        case value.oneTransfer === 'false' && value.withoutTransfer === 'true': {
-          const filter = data.filter((i) => i);
-        }
-        case value.oneTransfer === 'false' && value.withoutTransfer === 'false': {
-          state.filteredData = action.payload;
-        }
-        default:
-          return state;
+    setFilteredData(state, action) {
+      const { payload } = action;
+      const tr__1 = state.oneTransfer;
+      const tr__0 = state.withoutTransfer;
+
+      const filterTranfers = (T = 0) => {
+        T = T + 1; // T - number of transfers
+        const legs = (f, L) => f.legs[L].segments.length; // L(e.g. legs) - flight direction, to - "0", back - "1"
+        return payload.filter(({ flight }) => legs(flight, 0) === T && legs(flight, 1) === T);
+      };
+
+      if (tr__1 === true && tr__0 === true) {
+      } else if (tr__1 === true && tr__0 === false) {
+        state.data = filterTranfers(1);
+      } else if (tr__1 === false && tr__0 === true) {
+        state.data = filterTranfers(0);
+      } else if (tr__1 === false && tr__0 === false) {
+        state.data = payload;
+      } else {
+        return state;
       }
     },
   },
@@ -42,12 +41,12 @@ export const slice = createSlice({
 
 export const { filterTransfers, setFilteredData } = slice.actions;
 
-export const selectFilteredData = (state) => state.filter.filteredData;
-export const selectFilteringBy = (state) => state.filter.filteringBy;
+export const selectFilterData = (state) => state.filter.data;
 
-export const filterFlights = (payload) => async (dispatch, getState) => {
+export const filterFlights = (payload) => (dispatch, getState) => {
   const { data } = selectData(getState());
   payload && dispatch(filterTransfers(payload));
+  dispatch(setFilteredData(data));
 };
 
 export default slice.reducer;
