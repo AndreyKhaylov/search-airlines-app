ca606838ca225cefd1ec1419c12b6df275914105
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-01-26 14:02:37 +0300

    Created useDebounce hook

--- src/components/searchParams/SettingPrice.jsx
+++ src/components/searchParams/SettingPrice.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import { settingPriceData } from '../../store/reducers/settingPriceReducer'
 
 export const SettingPrice = () => {
-    // const [range, setPrice] = React.useState({
-    //     down: 0,
-    //     up: 0,
-    // })
+    const [range, setPrice] = React.useState({
+        down: 0,
+        up: 0,
+    })
 
-    const onSettingPrice = (e) => {
-        const { value } = e.target
-        dispatch(settingPriceData(range))
-    }
+    const onSettingPrice = useCallback(
+        async(e) => {
+            const { value } = e.target
+            setPrice(value)
+            dispatch(settingPriceData(range))
+        }, [])    
 
     return (
         <div>
