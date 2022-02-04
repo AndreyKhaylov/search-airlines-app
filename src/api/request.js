2782b588b87ac92be12c009415282313a2b57b01
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-02-04 21:19:57 +0300

    [request.js]: corrected url address

--- src/api/request.js
+++ src/api/request.js
@@ -1,7 +1,7 @@
-const URL = 'http://localhost:3001/result/';
+const BASE_URL = 'http://localhost:3001/result/';
 
 export const requestAPI = async () => {
-  const response = await fetch(URL)
+  const response = await fetch(BASE_URL)
     .then((res) => res.json())
     .catch((err) => console.log('error', err));
 
