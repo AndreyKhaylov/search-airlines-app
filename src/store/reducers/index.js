1a5a2752a6365919e35629daae51b756e14e00e6
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-02-09 19:55:18 +0300

    Created index.js in reducers for imports

---
+++ src/store/reducers/index.js
@@ -0,0 +1,15 @@
+import dataFlights from './dataFlights';
+import filtrationTranfers from './filtrationTranfers';
+import filtrationAirlines from './filtrationAirlines';
+import filtrationPrice from './filtrationPrice';
+import sortingData from './sortingData';
+import pagination from './pagination';
+
+export {
+  dataFlights,
+  filtrationTranfers,
+  filtrationAirlines,
+  filtrationPrice,
+  sortingData,
+  pagination,
+};
