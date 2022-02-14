03b7aeea521bb3d3b862fb0d3d39a2839ad1df80
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-02-14 12:25:34 +0300

    [styles]: added styles in filterAirline.jsx

--- src/components/searchParams/FilterAirline.jsx
+++ src/components/searchParams/FilterAirline.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { filtrationAirlines } from '../../store/reducers/filtrationAirlines';
 import { selectData } from '../../store/reducers/dataFlights';
 
+import { FormLabel, FormControl, FormGroup, FormControlLabel, Checkbox } from '@mui/material/';
+
 const AIRLINE = 'Авикомпания';
 
 export const FilterAirline = () => {
@@ -17,28 +19,32 @@ export const FilterAirline = () => {
     }
 
     return (
-        <div>
-            <h4>{AIRLINE}</h4>
-            {
-                data.map(({ flight: { carrier }}, i) => {
-                    if(!arr.includes(carrier.caption)) {
-                        arr.push(carrier.caption)
-                        return (
-                            <div key={i}>
-                                <label>
-                                    <input
-                                        type='checkbox'
-                                        name='select'
-                                        value={carrier.caption}
-                                        onChange={onSetAirlines}
-                                    />
-                                    - ({carrier.airlineCode}) {carrier.caption}
-                                </label>
-                            </div>
-                        )
-                    }  
-                })
-            }
-        </div>
+        <FormControl sx={{ mt: 4 }} >
+            <FormLabel >{ AIRLINE }</FormLabel>
+            <FormGroup sx={{ m: 2 }}>
+                {
+                    data.map(({ flight: { carrier }}, i) => { 
+                        if(!arr.includes(carrier.caption)) {
+                            arr.push(carrier.caption)
+                            return (
+                                <FormControlLabel 
+                                    key={ i }
+                                    value={ carrier.caption }
+                                    onChange={ onSetAirlines }
+                                    control={ <Checkbox size="small"/> }
+                                    label={ `- (${carrier.airlineCode}) ${carrier.caption}` }
+                                    labelPlacement="end"
+                                />
+                            )
+                        }
+                        return null 
+                    })
+                }
+            </FormGroup>
+        </FormControl>
     )
 }
+
+// this.props.comments
+//   .filter(commentReply => commentReply.replyTo === comment.id)
+//   .map((commentReply, idx) => <CommentItem key={idx} className="SubComment"/>);
