ae5f75573116614955afbe89a081ffe6ac11b7b2
Andrey Khaylov <andrey.haylovv@yandex.ru>
2022-02-26 15:01:47 +0300

    [refactoring]: changed type button naming

--- src/components/flightsList/FlightsList.jsx
+++ src/components/flightsList/FlightsList.jsx
@@ -7,27 +7,31 @@ import { CustomButton } from '../button/button';
 
 import Box from '@mui/material/Box';
 
+const title = {
+  selectBtn: 'Выбрать',
+  showBtn: 'Показать еще',
+};
+
 export const FlightsList = () => {
-    const flights = useSelector(selectData)
-    const dispatch = useDispatch()
+  const flights = useSelector(selectData);
+  const dispatch = useDispatch();
 
-    const onHandleClick = () => {
-        dispatch(pagination(1))
-    }
+  const onHandleClick = () => {
+    dispatch(pagination(1));
+  };
 
-    return (
-        <Box sx={{ flexDirection: 'column' }}>
-            {flights && flights.map(({ flight, flightToken }, idx) => (
-                <section key={ idx }>
-                    <FlightInfo {...flight} />
-                    <CustomButton onClick={() => console.log('select', flightToken)}>
-                        Выбрать
-                    </CustomButton>
-                </section>
-            ))}
-            <CustomButton onClick={ onHandleClick }>
-                Показать еще
+  return (
+    <Box sx={{ flexDirection: 'column' }}>
+      {flights &&
+        flights.map(({ flight, flightToken }, idx) => (
+          <section key={idx}>
+            <FlightInfo {...flight} />
+            <CustomButton onClick={() => console.log('select', flightToken)}>
+              {title.selectBtn}
             </CustomButton>
-        </Box>
-    )
-}
+          </section>
+        ))}
+      <CustomButton onClick={onHandleClick}>{title.showBtn}</CustomButton>
+    </Box>
+  );
+};
